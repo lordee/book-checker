50cb23b7a62fdf2fa5171414b0db127349177a80
lordee <7160918+lordee@users.noreply.github.com>
2026-05-07 10:28:02 +0930

    update to handle ingress paths

--- book_checker/next.config.ts
+++ book_checker/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  basePath: process.env.NODE_ENV === 'production' ? '/api/hassio_ingress/book_checker' : '',
+  trailingSlash: true,
   images: {
     remotePatterns: [
       {
